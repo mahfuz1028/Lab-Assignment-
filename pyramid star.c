00ba00bc6463e03aceb6ad4156c74a912942eb67
mahfuz1028 <106919422+mahfuz1028@users.noreply.github.com>
2022-06-05 15:40:58 +0000

    Add files via upload

---
+++ pyramid star.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+int n,row,column;
+printf("enter ur special number:");
+scanf("%d",&n);
+for(row=1;row<=n;row++)
+{
+  for(column=1;column<=n-row;column++)
+  printf(" ");
+  for(column=1;column<=2*row-1;column++)
+  printf("*");
+
+  printf("\n");
+}
+}
